1aa207b086e033111dff22d99351fe41b3fdc2d4
Ian Bytchek <ianbytchek@gmail.com>
2020-06-07 14:07:01 +0100

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "Slang",
+    platforms: [
+        .macOS(.v10_12)
+    ],
+    products: [
+        .library(name: "Slang", targets: ["Slang"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.29.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
+        .package(url: "https://github.com/Quick/Quick.git", from: "2.0.0"),
+        .package(url: "https://github.com/sharplet/Regex.git", from: "2.1.0"),
+    ],
+    targets: [
+        .target(name: "Slang", dependencies: ["Regex", "SourceKittenFramework"], path: "source/Slang", exclude: ["Test"]),
+        .testTarget(name: "Slang-Test", dependencies: ["Slang", "Quick", "Nimble"], path: "source/Slang/Test"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
